d721fdc4e683c53e737d9e1784877369d79ce337
andre <60292382+Andre-06@users.noreply.github.com>
2023-07-06 17:07:12 -0300

    Add files via upload

---
+++ CreateCharacterController.java
@@ -0,0 +1,47 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CreateCharacterController {
+
+    @FXML
+    private Button backgroundCharacterButton;
+
+    @FXML
+    private Button classCharacterButton;
+
+    @FXML
+    private Button createCharacterButton;
+
+    @FXML
+    private Button dateCharacterButton;
+
+    @FXML
+    private Button raceCharacterButton;
+
+    @FXML
+    void backgroundCharacterButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void classCharacterButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createCharacterButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dateCharacterButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void raceCharacterButtonAction(ActionEvent event) {
+
+    }
+
+}
