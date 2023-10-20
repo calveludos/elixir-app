aa3432f3fc1c4a60ac67fe6620b6e40239b3045a
Andre Araujo <andre.araujosilva2006@gmail.com>
2023-10-19 23:32:33 -0300

    Remerge

--- src/main/java/com/elixir/controller/ViewCharacterPage1Controller.java
+++ src/main/java/com/elixir/controller/ViewCharacterPage1Controller.java
@@ -1,5 +1,6 @@
 package com.elixir.controller;
 
+import com.elixir.manager.JsonManger;
 import com.elixir.manager.ObjectSaveManager;
 import com.elixir.manager.PaneManager;
 import com.elixir.model.Attribute;
@@ -7,12 +8,13 @@ import com.elixir.model.Character;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
 import java.util.Locale;
 
 public class ViewCharacterPage1Controller {
@@ -78,7 +80,7 @@ public class ViewCharacterPage1Controller {
     private TextField learnMagicField;
 
     @FXML
-    private Spinner<?> levelSpinner;
+    private Spinner<Integer> levelSpinner;
 
     @FXML
     private TextField maxPvField;
@@ -122,8 +124,10 @@ public class ViewCharacterPage1Controller {
     private Character character;
     private Attribute attribute;
 
+    private int level;
+
     @FXML
-    public void initialize() {
+    public void initialize() throws IOException, ParseException {
         ObjectSaveManager reader = new ObjectSaveManager();
         character = (Character) reader.getObject("character");
         attribute = (Attribute) reader.getObject("attribute");
@@ -140,6 +144,38 @@ public class ViewCharacterPage1Controller {
         String clas = MyCharactersController.getClassId(character.getClassId());
         String race = MyCharactersController.getRaceId(character.getRaceId());
         classAndRaceField.setText(clas.toUpperCase() + " / " + race.toUpperCase());
+
+        JSONArray levelsArray;
+
+        try {
+            levelsArray = (JSONArray) JsonManger.get("class/" + CreateCharacterBackgroundController.getClass(character.getClassId()) + "/level");
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        long maxXp = 0;
+        for (Object json :
+                levelsArray) {
+            JSONObject jsonObject = (JSONObject) json;
+            System.out.println("max xp " + maxXp);
+            System.out.println("char xp " + character.getExperience());
+            if (character.getExperience() <= maxXp){
+                level = Integer.parseInt(String.valueOf((long) jsonObject.get("Nível"))) - 1;
+                break;
+            } else {
+                maxXp = (long) jsonObject.get("XP");
+            }
+        }
+        if (level == 0){
+            level = 20;
+        }
+
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 20, 1);
+        levelSpinner.setValueFactory(valueFactory);
+        levelSpinner.getValueFactory().setValue(level);
+        appereanceField.setText(character.getAppearance());
     }
 
     private void setAttributes() {
@@ -151,7 +187,15 @@ public class ViewCharacterPage1Controller {
         wisField.setText(String.valueOf(attribute.getWisdom()));
     }
 
-    private void setSubAttributes() {
+    private void setSubAttributes() throws IOException, ParseException {
+        maxPvField.setText(String.valueOf(character.getMaxPv()));
+        currentPvField.setText(String.valueOf(character.getCurrentPv()));
+        System.out.println("Level" + level);
+        String mv = String.valueOf((long) JsonManger.get("race/race:" + character.getRaceId() + "/moveBase" ));
+        String jp = String.valueOf((long) JsonManger.get("class/" + CreateCharacterBackgroundController.getClass(character.getClassId()) + "/level:" + level + "/Jogada de Proteção"));
+
+        mvField.setText(mv);
+        jpField.setText(jp);
     }
 
     private void setAjusts() {
@@ -688,6 +732,9 @@ public class ViewCharacterPage1Controller {
 
     @FXML
     void backButtonAction(ActionEvent event) {
+        ObjectSaveManager saveManager = new ObjectSaveManager();
+        saveManager.removeObject("character");
+
         PaneManager manager = new PaneManager();
         manager.openPane("myCharactersPane");
     }
